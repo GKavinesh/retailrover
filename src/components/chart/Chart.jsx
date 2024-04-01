4251a796d805efdbc1e5890e461fd2df3e795e28
GKavinesh <ganeshamoorthykavinesh186@gmail.com>
2024-04-01 07:37:13 +0530

    Commit 2

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,16 @@
+import "./chart.scss";
+import Image from "../images/ganesha food.png";
+const Chart = ({ aspect, title }) => {
+  return (
+    <div className="chart">
+      <div className="title">{title}</div>
+      <div className="welcome">
+        <img src={Image} alt="Welcome" />
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
+
+
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,81 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PaidIcon from '@mui/icons-material/Paid';
+import StoreIcon from "@mui/icons-material/Store";
+import GroupIcon from '@mui/icons-material/Group';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+
+const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">MavenTek</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PaidIcon className="icon" />
+              <span>Billing</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreIcon className="icon" />
+              <span>Inventory</span>
+            </li>
+          </Link>
+          <li>
+            <GroupIcon className="icon" />
+            <span>Suppliers</span>
+          </li>
+          <li>
+            <PersonIcon className="icon" />
+            <span>Customers</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
